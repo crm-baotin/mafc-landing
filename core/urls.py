d54747f90fae933b7fad49b4a452e101e22ad19a
Bao Tin <baotin1991@gmail.com>
2026-01-03 15:04:02 +0700

    Fix static files path and collectstatic

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from leads import views
 
 urlpatterns = [
+    path('', views.landing_mafc, name='home'),  # 👈 ROOT
+    path('mafc/', include('leads.urls')),       # vẫn giữ nếu cần
     path('admin/', admin.site.urls),
-    path('', include('leads.urls')),
 ]
